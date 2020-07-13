559f033f679f974a4a0ff9153fcbe6a09583a233
bharath0080 <bharathforany@gmail.com>
2020-07-13 15:00:57 +0530

    Update shrd.groovy

--- vars/shrd.groovy
+++ vars/shrd.groovy
@@ -2,3 +2,7 @@ def test(Map pipeline){
         println pipeline.name
         println pipeline.company
 }
+def test1(Map pipeline){
+        println pipeline.name1
+        println pipeline.company1
+}
